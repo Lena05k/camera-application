85e1c42fcd858a89f270077025e4ac92c4637e5d
Lena05k <lena05k@yandex.ru>
2023-07-09 23:20:14 +0300

    Added modal map

---
+++ frontend/src/components/UI/modal/ModalMap.jsx
@@ -0,0 +1,22 @@
+import {
+  YMaps,
+  Map,
+  FullscreenControl,
+  GeolocationControl,
+  TypeSelector,
+  ZoomControl,
+} from '@pbe/react-yandex-maps';
+import React from 'react';
+
+const ModalMap = () => (
+  <YMaps className="rounded">
+    <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }}>
+      <FullscreenControl />
+      <GeolocationControl options={{ float: "right" }} />
+      <ZoomControl options={{ float: "right" }} />
+      <TypeSelector options={{ float: "right" }} />
+    </Map>
+  </YMaps>
+);
+
+export default ModalMap;

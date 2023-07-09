58f928b327118b89a6caedd0c7b6580236200636
Lena05k <lena05k@yandex.ru>
2023-07-09 23:17:30 +0300

    Added button for map

--- frontend/src/components/UI/button/ButtonMap.jsx
+++ frontend/src/components/UI/button/ButtonMap.jsx
@@ -1,23 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button } from 'react-bootstrap';
 import * as SlIcons from 'react-icons/sl';
-import ModalMap from '../UI/modal/ModalMap';
 
-const ButtonMap = () => {
-  const [showMap, setShowMap] = useState(false);
-
-  const handleCloseMap = () => setShowMap(showMap);
-  const handleShowMap = () => setShowMap(!showMap);
-  console.log(setShowMap);
-
-  return (
-    <>
-      <Button variant="outline-light" onClick={handleShowMap}>
-        <SlIcons.SlMagnifierAdd className="fs-2" />
-      </Button>
-      {showMap && <ModalMap show={showMap} closeModal={handleCloseMap} />}
-    </>
-  );
-};
+const ButtonMap = ({ openModal }) => (
+  <Button variant="outline-light" className="rounded border border-2" onClick={ () => openModal(true)}>
+    <SlIcons.SlMagnifierAdd className="fs-2" />
+  </Button>
+);
 
 export default ButtonMap;

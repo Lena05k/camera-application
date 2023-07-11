2c8f2178d5d9130857f6740d4ebf1c3e70aa1ae4
Lena05k <lena05k@yandex.ru>
2023-07-11 20:44:53 +0300

    Added button for photo

---
+++ frontend/src/components/UI/button/ButtonPhoto.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+const ButtonPhoto = () => (
+    <Button
+        variant="link"
+        className="text-end text-center mb-4 btn text-white link-offset-2 link-underline link-underline-opacity-0"
+    >
+        ФОТО
+    </Button>
+);
+
+export default ButtonPhoto;

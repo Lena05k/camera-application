65a10de2c1ccd9d068cf1d0db085458cbbc12fe1
Lena05k <lena05k@yandex.ru>
2023-07-11 20:45:57 +0300

    Added button for video

---
+++ frontend/src/components/UI/button/ButtonVideo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const ButtonVideo = () => (
+    <Button
+        variant="link"
+        className="text-end text-center mt-4 btn text-white link-offset-2 link-underline link-underline-opacity-0"
+    >
+        ВИДЕО
+    </Button>
+);
+
+export default ButtonVideo;

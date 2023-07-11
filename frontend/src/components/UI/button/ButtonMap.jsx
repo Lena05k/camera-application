c286a1bb9ff67bb62c9f65eb2106513d7a399904
Lena05k <lena05k@yandex.ru>
2023-07-11 20:43:28 +0300

    Added a transparent gray background

--- frontend/src/components/UI/button/ButtonMap.jsx
+++ frontend/src/components/UI/button/ButtonMap.jsx
@@ -3,7 +3,7 @@ import { Button } from 'react-bootstrap';
 import * as SlIcons from 'react-icons/sl';
 
 const ButtonMap = ({ openModal }) => (
-  <Button variant="outline-light" className="rounded border border-2" onClick={ () => openModal(true)}>
+  <Button variant="outline-light" className="rounded border border-2 bg-opacity-50 bg-secondary" onClick={() => openModal(true)}>
     <SlIcons.SlMagnifierAdd className="fs-2" />
   </Button>
 );

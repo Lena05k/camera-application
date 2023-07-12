8cf39b66d451c8a317879578258ee80918b92594
Lena05k <lena05k@yandex.ru>
2023-07-12 22:52:22 +0300

    Added router /gallery

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -5,17 +5,18 @@ import {
   Route,
 } from 'react-router-dom';
 import routes from './routes/routes';
-// import 'react-toastify/dist/ReactToastify.css';
 import MainPage from './pages/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.css';
 import CameraPage from './pages/CameraPage';
+import GalleryPage from './pages/GalleryPage';
 
 const App = () => (
   <Router>
       <Routes>
         <Route path={routes.root()} element={<MainPage />} />
         <Route path={routes.camera()} element={<CameraPage />} />
+        <Route path={routes.gallery()} element={<GalleryPage />} />
       </Routes>
   </Router>
 );

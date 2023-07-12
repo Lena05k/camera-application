2d799ae94ea9b95f8d62d6694e5ad7ad00093b1f
Lena05k <lena05k@yandex.ru>
2023-07-12 18:56:04 +0300

    Added style

--- frontend/src/components/pages/MainPage.jsx
+++ frontend/src/components/pages/MainPage.jsx
@@ -5,15 +5,15 @@ import { Link } from 'react-router-dom';
 const MainPage = () => {
   return (
     <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center">
-      <Card className="col-12 col-md-8 col-xxl-6 ">
+      <Card className="col-12 col-md-8 col-xxl-6" style={{ background: '#212529' }}>
         <Card.Body className="col p-5 text-center">
           <Link to="/camera" variant="outline-primary justify-content-center">
-            <Button variant="primary" className="m-2 fs-1 p-5">
+            <Button variant="secondary" className="m-2 fs-1 p-5">
               Камера
             </Button>
           </Link>
           <Link to="/gallery" variant="outline-primary justify-content-center align-items-center">
-            <Button variant="primary" className="p-5 m-2 fs-1">
+            <Button variant="secondary" className="p-5 m-2 fs-1">
               Галерея
             </Button>
           </Link>

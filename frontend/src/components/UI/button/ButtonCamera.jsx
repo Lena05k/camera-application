df34c266a4b5fe380bb09aab4ebba98644c212e3
Lena05k <lena05k@yandex.ru>
2023-07-12 18:52:12 +0300

    ...

--- frontend/src/components/UI/button/ButtonCamera.jsx
+++ frontend/src/components/UI/button/ButtonCamera.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const ButtonCamera = () => (
-    <div
-        className="d-flex align-self-center rounded-circle border border-1 border-white"
-        style={{ width: '120px', height: '120px' }}
-    >
-        <Button
-            className="align-self-center ms-2 rounded-circle bg-white border border-2"
-            style={{ width: '100px', height: '100px' }}
-        />
-    </div>
+const ButtonCamera = ({ startShooting }) => (
+  <Button
+    onClick={() => startShooting(true)}
+    className="align-self-center ms-2 rounded-circle bg-white border border-2"
+    style={{ width: '100px', height: '100px' }}
+  />
 );
 
 export default ButtonCamera;

1bb3e22b2d63a842d95b588a0945df2379b0e668
Lena05k <lena05k@yandex.ru>
2023-07-12 22:53:19 +0300

    ...

--- frontend/src/components/blocks/BlockShooting.jsx
+++ frontend/src/components/blocks/BlockShooting.jsx
@@ -3,15 +3,11 @@ import ButtonPhoto from '../UI/button/ButtonPhoto';
 import ButtonVideo from '../UI/button/ButtonVideo';
 
 const BlockShooting = () => {
-
-
   return (
-      <>
-          <div className="d-flex flex-column top-50 end-0 ms-3">
-              <ButtonPhoto />
-              <ButtonVideo />
-          </div>
-      </>
+    <div className="d-flex flex-column top-50 end-0 ms-3">
+      <ButtonPhoto />
+      <ButtonVideo />
+    </div>
   );
 };
 

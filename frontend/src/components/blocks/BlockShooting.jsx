541772ba1b010a13de379ee9f4dd3b9f54bfb74a
Lena05k <lena05k@yandex.ru>
2023-07-15 18:27:03 +0300

    Added handleOpenPhoto, handleOpenVideo

--- frontend/src/components/blocks/BlockShooting.jsx
+++ frontend/src/components/blocks/BlockShooting.jsx
@@ -1,12 +1,37 @@
-import React, {useState} from 'react';
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
 import ButtonPhoto from '../UI/button/ButtonPhoto';
 import ButtonVideo from '../UI/button/ButtonVideo';
 
-const BlockShooting = () => {
+const BlockShooting = ({ openShootingView, activeButton, setActiveButton }) => {
+  const handleOpenPhoto = () => {
+    openShootingView(true);
+    setActiveButton('camera');
+  };
+
+  const handleOpenVideo = () => {
+    openShootingView(false);
+    setActiveButton('video');
+  };
+
   return (
     <div className="d-flex flex-column top-50 end-0 ms-3">
-      <ButtonPhoto />
-      <ButtonVideo />
+      <ButtonPhoto
+        openShootingView={handleOpenPhoto}
+        isActive={activeButton === 'camera'}
+      />
+      <ButtonVideo
+        openShootingView={handleOpenVideo}
+        isActive={activeButton === 'video'}
+      />
+      {
+        activeButton === 'video' && (
+          <FaIcons.FaCircle
+            className="position-absolute fs-1 text-danger"
+            style={{ top: '40px', left: '40px' }}
+          />
+        )
+      }
     </div>
   );
 };

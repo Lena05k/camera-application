c2fd15bd1e6ac3990e4f901d8b7aa74758e0b641
Lena05k <lena05k@yandex.ru>
2023-07-15 18:25:12 +0300

    Added style fw-bold

--- frontend/src/components/UI/button/ButtonPhoto.jsx
+++ frontend/src/components/UI/button/ButtonPhoto.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-const ButtonPhoto = () => (
-    <Button
-        variant="link"
-        className="text-end text-center mb-4 btn text-white link-offset-2 link-underline link-underline-opacity-0"
-    >
-        ФОТО
-    </Button>
+
+const ButtonPhoto = ({ openShootingView, isActive }) => (
+  <Button
+    onClick={openShootingView}
+    id="camera"
+    variant="link"
+    className={`text-end text-center mt-2 btn text-white link-offset-2 link-underline link-underline-opacity-0 ${
+      isActive ? 'fw-bold' : ''
+    }`}
+  >
+    ФОТО
+  </Button>
 );
 
 export default ButtonPhoto;

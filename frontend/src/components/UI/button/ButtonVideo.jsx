e337deab536d27c4a6e68c5070cc82ca6b92b723
Lena05k <lena05k@yandex.ru>
2023-07-15 18:25:51 +0300

    Added style fw-bold

--- frontend/src/components/UI/button/ButtonVideo.jsx
+++ frontend/src/components/UI/button/ButtonVideo.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const ButtonVideo = () => (
-    <Button
-        variant="link"
-        className="text-end text-center mt-4 btn text-white link-offset-2 link-underline link-underline-opacity-0"
-    >
-        ВИДЕО
-    </Button>
+const ButtonVideo = ({ openShootingView, isActive }) => (
+  <Button
+    onClick={openShootingView}
+    id="video"
+    variant="link"
+    className={`text-end text-center mt-4 btn text-white link-offset-2 link-underline link-underline-opacity-0 ${
+      isActive ? 'fw-bold' : ''
+    }`}
+  >
+    ВИДЕО
+  </Button>
 );
 
 export default ButtonVideo;

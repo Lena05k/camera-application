5728961669feb5a096ebbd8f0d293ade3b1f8066
Lena05k <lena05k@yandex.ru>
2023-07-20 20:38:44 +0300

    Changed the style

--- frontend/src/components/pages/MainPage/bloks/BlockModalSettings.jsx
+++ frontend/src/components/pages/MainPage/bloks/BlockModalSettings.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import ModalSetting from '../../UI/modal/ModalSetting';
-import ButtonSettings from './ButtonSettings';
+import ModalSetting from '../../../UI/settings/modal/ModalSetting';
+import ButtonSettings from '../buttons/ButtonSettings';
 
 const BlockModalSettings = () => {
   const [modalShow, setModalShow] = useState(false);
@@ -8,7 +8,7 @@ const BlockModalSettings = () => {
 
   return (
     <>
-      <div className="position-absolute top-0 end-0 mt-5 me-5 bg-black">
+      <div className="position-absolute top-0 end-0 mt-5 me-5 bg-black bg-opacity-25">
         <ButtonSettings setModalShow={handleModalShow} />
       </div>
       <ModalSetting
--- frontend/src/components/pages/MainPage/buttons/ButtonSettings.jsx
+++ frontend/src/components/pages/MainPage/buttons/ButtonSettings.jsx
@@ -3,7 +3,7 @@ import { Button } from 'react-bootstrap';
 import React from 'react';
 
 const ButtonSettings = ({ setModalShow }) => (
-  <Button variant="outline-light" onClick={() => setModalShow(true)}>
+  <Button variant="outline-light" className="bg-opacity-25" onClick={() => setModalShow(true)}>
     <FiIcons.FiSettings className="fs-3" />
   </Button>
 );

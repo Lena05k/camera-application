71234653184a2a51293ce65533cd4868c2925c24
Lena05k <lena05k@yandex.ru>
2023-07-20 20:37:13 +0300

    Added table in report

--- frontend/src/components/pages/RepostPage/ReportPage.jsx
+++ frontend/src/components/pages/RepostPage/ReportPage.jsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Card } from 'react-bootstrap';
 import Table from './Table/Table';
+import Header from './bloks/Header';
 
 const ReportPage = () => {
 
   return (
     <div className="position-relative d-flex flex-column min-vh-100 justify-content-center align-items-center">
+      <Header />
       <div className="">
         <Card className="col p-5 text-center">
           <Card.Body>

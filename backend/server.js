32413cf18df8e59d7b3bbb64db376e8790bb3409
Lena05k <lena05k@yandex.ru>
2023-10-20 08:33:07 +0300

    Added server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const app = express();
+const mysql = require('mysql');
+
 
 app.get('/', (req, res) => {
   res.send('HELLO POSTGRES + NODEJS!!');

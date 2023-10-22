cae3d28c2dd1c365788e86d38452467ac5a408e6
Lena05k <lena05k@yandex.ru>
2023-10-22 11:40:49 +0300

    Added express.json

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,21 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const mysql = require('mysql');
+const db = require('./models');
+db.sequelize.sync()
+    .then(() => {
+      console.log('Synced db.');
+    })
+    .catch((err) => {
+      console.log('Failed to syns db: ' + err.message);
+    });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('HELLO POSTGRES + NODEJS!!');
+    res.json({requestBody: req.body})
+    res.send('HELLO POSTGRES + NODEJS!!');
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

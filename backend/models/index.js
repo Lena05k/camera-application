a902707baa453f2024e721ad6e9b1234570531ea
Lena05k <lena05k@yandex.ru>
2023-10-22 11:46:02 +0300

    Added a connection: one to one belongsTo(), one to many hasMany()

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,43 +1,58 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
-const basename = path.basename(__filename);
+const {DataTypes} = require("sequelize");
+// const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const dbConfig = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (dbConfig.use_env_variable) {
+  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], dbConfig);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
 }
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+db.Repost = require('./ReportModule')(sequelize, DataTypes);
+db.User = require('./UserModul')(sequelize, DataTypes);
+db.WeatherForecast = require('./WeatherForecastModule')(sequelize, DataTypes);
+db.Location = require('./LocationModule')(sequelize, DataTypes);
+db.Defects = require('./DefectsModule')(sequelize, DataTypes);
+db.Recommendation = require('./RecommendationModule')(sequelize, DataTypes);
+db.Results = require('./ResultsModule')(sequelize, DataTypes);
+db.Gallery = require('./GalleryModule')(sequelize, DataTypes);
+db.Photo = require('./PhotoModule')(sequelize, DataTypes);
+db.Video = require('./VideoModule')(sequelize, DataTypes);
+db.Data = require('./DataModule')(sequelize, DataTypes);
+
+// 1 - N Пользователь может бесконечно делать фото и видео
+db.User.hasMany(db.Photo, { as: 'photo', foreignKey: 'id_photo'});
+db.User.hasMany(db.Video, { as: 'video', foreignKey: 'id_video'});
+
+// 1 - 1 Фото или видео првязаны к дату
+db.Photo.belongsTo(db.Data);
+db.Video.belongsTo(db.Data);
+
+// 1 - N Пользователь может сколько угодно создавать id_отчет
+db.User.hasMany(db.Repost, { as: 'repost', foreignKey: 'id_repost' });
+
+// 1 - 1 Отчет принадлежит только пользователю
+db.Repost.belongsTo(db.User, { as: 'user', foreignKey: 'id_user'});
+
+// 1 - 1 Данные принадлежит id_отчету
+db.WeatherForecast.belongsTo(db.Repost, { as: 'repost', foreignKey: 'id_repost' });
+db.Location.belongsTo(db.Repost, { as: 'repost', foreignKey: 'id_repost' });
+db.Defects.belongsTo(db.Repost, { as: 'repost', foreignKey: 'id_repost' });
+db.Recommendation.belongsTo(db.Repost, { as: 'repost', foreignKey: 'id_repost' });
+db.Results.belongsTo(db.Repost, { as: 'repost', foreignKey: 'id_repost' });
+db.Photo.belongsTo(db.Repost, { as: 'repost', foreignKey: 'id_repost' });
+db.Video.belongsTo(db.Repost, { as: 'repost', foreignKey: 'id_repost' });
 
 module.exports = db;

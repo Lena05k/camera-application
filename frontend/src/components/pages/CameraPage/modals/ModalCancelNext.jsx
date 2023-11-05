7632731a2dd606891bbb1b2aa74c6c368af28cd5
Lena05k <lena05k@yandex.ru>
2023-11-05 13:20:44 +0300

    Added modal cancel & next

---
+++ frontend/src/components/pages/CameraPage/modals/ModalCancelNext.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Button,
+  Modal,
+} from 'react-bootstrap';
+
+
+const ModalCancelNext = () => {
+
+  return (
+    <Modal
+      size="sm"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Body>
+        <h3>СНЯТО! Создать отчет?</h3>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" >Отмена</Button>
+        <Button variant="primary" >Далее</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+};
+
+export default ModalCancelNext;

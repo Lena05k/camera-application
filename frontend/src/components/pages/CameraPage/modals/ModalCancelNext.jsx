18031b45b3c74c9d212e402b2f48bc00e075b153
Lena05k <lena05k@yandex.ru>
2023-11-09 13:29:29 +0300

    Added modal from repost

--- frontend/src/components/pages/CameraPage/modals/ModalCancelNext.jsx
+++ frontend/src/components/pages/CameraPage/modals/ModalCancelNext.jsx
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   Modal,
 } from 'react-bootstrap';
+import ModalFormRepost from './ModalFormRepost';
 
 
 const ModalCancelNext = (props) => {
+  const [showModal, setShow] = useState(false);
+
+  const handleCloseModal = () => setShow(false);
+  const handleModalNext = () => setShow(true);
 
   return (
-    <Modal
-      { ...props }
-      size="md"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Body>
-        <h3>СНЯТО! Создать отчет?</h3>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" >Отмена</Button>
-        <Button variant="primary" >Далее</Button>
-      </Modal.Footer>
-    </Modal>
+    <>
+      <Modal
+        { ...props }
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Body>
+          <h3>СНЯТО! Создать отчет?</h3>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>Отмена</Button>
+          <Button variant="primary" onClick={handleModalNext}>Далее</Button>
+        </Modal.Footer>
+      </Modal>
+      <ModalFormRepost show={showModal} onHide={handleCloseModal} />
+    </>
   )
 };
 
--- frontend/src/components/pages/CameraPage/modals/ModalFormRepost.jsx
+++ frontend/src/components/pages/CameraPage/modals/ModalFormRepost.jsx
@@ -11,75 +11,63 @@ import {
 const ModalFormRepost = (props) => {
   return (
     <Modal
-      { ...props }
-      size="md"
+      {...props}
+      size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
       <Modal.Body>
         <Form>
-          <Row>
+          <Row className="mb-3">
             <Form.Group as={Col} controlId="formGridEmail">
               <Form.Label>Цех</Form.Label>
-              <Form.Control type="text" placeholder="Укажите адрес цеха" />
+              <Form.Control type="text" placeholder="ЦЭС-3"/>
             </Form.Group>
             <Form.Group as={Col} controlId="formGridEmail">
               <Form.Label>Опора</Form.Label>
-              <Form.Control type="text" placeholder="Укажите номер опоры" />
+              <Form.Control type="text" placeholder="EL52"/>
             </Form.Group>
-          </Row>
-          <Row>
             <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>Цех</Form.Label>
-              <Form.Control type="text" placeholder="Укажите адрес цеха" />
+              <Form.Label>Коррдинаты</Form.Label>
+              <Form.Control type="text" placeholder="55.778186, 38.676863"/>
             </Form.Group>
           </Row>
         </Form>
-        <Row>
+        <Row className="mb-3">
           <Form.Group as={Col} controlId="formGridEmail">
             <Form.Label>Температура</Form.Label>
-            <Form.Control type="text" placeholder="Укажите температуру" />
+            <Form.Control type="text" placeholder="&#176;С"/>
           </Form.Group>
           <Form.Group as={Col} controlId="formGridEmail">
             <Form.Label>Давление</Form.Label>
-            <Form.Control type="text" placeholder="Укажите давление" />
+            <Form.Control type="text" placeholder="гПа"/>
           </Form.Group>
-        </Row>
-        <Row>
           <Form.Group as={Col} controlId="formGridEmail">
             <Form.Label>Ветер</Form.Label>
-            <Form.Control type="text" placeholder="Укажите м/с" />
+            <Form.Control type="text" placeholder=" м/с"/>
           </Form.Group>
           <Form.Group as={Col} controlId="formGridEmail">
             <Form.Label>Влажность</Form.Label>
-            <Form.Control type="text" placeholder="Укажите влажность в %" />
+            <Form.Control type="text" placeholder="%"/>
           </Form.Group>
         </Row>
-        <Row>
-          <FloatingLabel controlId="floatingTextarea2" label="Результат">
-            <Form.Control
-              as="textarea"
-              placeholder="Результат"
-              style={{ height: '100px' }}
-            />
-          </FloatingLabel>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="floatingTextarea">
+            <Form.Control type="textarea" placeholder="Результат"/>
+          </Form.Group>
         </Row>
-        <Row>
-          <FloatingLabel controlId="floatingTextarea2" label="Рекомендация">
-            <Form.Control
-              as="textarea"
-              placeholder="Рекомендация"
-              style={{ height: '100px' }}
-            />
-          </FloatingLabel>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="floatingTextarea">
+            <Form.Control type="textarea" placeholder="Рекомендация"/>
+          </Form.Group>
         </Row>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" >Назад</Button>
-        <Button variant="primary" >Далее</Button>
+        <Button variant="secondary">Назад</Button>
+        <Button variant="primary">Далее</Button>
       </Modal.Footer>
     </Modal>
-  )
+  );
 };
 
 export default ModalFormRepost;

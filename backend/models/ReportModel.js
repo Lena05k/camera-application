7c6a06af141546e857b71da04f038e4a92ef750c
Lena05k <lena05k@yandex.ru>
2023-11-13 12:08:14 +0300

    ...

--- backend/models/ReportModel.js
+++ backend/models/ReportModel.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-    const Report = sequelize.define("Repost", {
+    const Report = sequelize.define('Repost', {
         id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
